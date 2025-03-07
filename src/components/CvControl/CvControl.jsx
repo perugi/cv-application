97321cf230482f5dd2c1e4381b27cbf134cc043c
perugi <dominik.perusko@gmail.com>
2025-03-07 18:24:29 +0100

    Add CV font size control

--- src/components/CvControl/CvControl.jsx
+++ src/components/CvControl/CvControl.jsx
@@ -1,9 +1,32 @@
+import PanelFrame from "../CvInput/Panels/PanelFrame/PanelFrame";
 import styles from "./CvControl.module.css";
 
 export default function CvControl({ className }) {
   return (
     <div className={`${className} ${styles.cvControl}`}>
-      CvControl
+      <PanelFrame title="CV Setup">
+        <div className={styles.fontSizeControl}>
+          <p>Font size:</p>
+          <button
+            className={`${styles.fontSizeButton} ${styles.active}`}
+            aria-label="Small font size"
+          >
+            Small
+          </button>
+          <button
+            className={styles.fontSizeButton}
+            aria-label="Medium font size"
+          >
+            Medium
+          </button>
+          <button
+            className={styles.fontSizeButton}
+            aria-label="Large font size"
+          >
+            Large
+          </button>
+        </div>
+      </PanelFrame>
     </div>
   );
 }
