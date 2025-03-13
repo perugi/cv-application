39ad2aae7a0e19a7fb0c766150ab53a14723bae7
perugi <dominik.perusko@gmail.com>
2025-03-13 10:01:44 +0100

    Prevent layout shift on the logo image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img src={logo} aria-label="CV Builder" />
+      <img src={logo} aria-label="CV Builder" width="500px" height="144.28px" />
     </header>
   );
 }
